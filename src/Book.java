4bd60788b6ffff8062cdd85d444fe52b87121a7c
Anna Szymanska <ania01081944@o2.pl>
2022-11-09 14:22:03 +0100

    Adding Book class and possibility to read file with books

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    String title ;
+    String author;
+    int pages;
+    int publishYear;
+    String genre;
+    String series ;
+    int seriesVolume;
+
+
+    public Book(String title, String author, int pages, int publishYear, String genre, String series, int seriesVolume){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publishYear = publishYear;
+        this.genre = genre;
+        this.series = series;
+        this.seriesVolume = seriesVolume;
+    }
+
+
+    // just for testing
+    public void description(){
+        if(series != null){
+            System.out.println("series - " + series);
+            System.out.println("volume of series - " + seriesVolume);
+        }
+        System.out.println("title - " + title);
+        System.out.println("author - " + author);
+        System.out.println("year of publishing - " + publishYear);
+        System.out.println("pages - " + pages);
+        System.out.println("genre - " + genre);
+    }
+}
---
+++ src/FileLoader.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileLoader {
+    public static void main(String []arg) throws FileNotFoundException {
+        test();
+    }
+    public static ArrayList<Book> returnBooksFromFile() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:\\Studia\\V semestr\\PIJAVA\\projekt\\Książki.csv"));
+        sc.useDelimiter(";");   //sets the delimiter pattern
+        sc.nextLine();
+        int counter = 1;
+        String title = null;
+        String series = null;
+        int seriesVolume = 0;
+        String author = null;
+        int pages = 0;
+        int publishYear = 0;
+        String genre = null;
+        String scanningResult;
+        ArrayList<Book> books = new ArrayList<>();
+        String find = "tom";
+
+
+        while (sc.hasNext())  //returns a boolean value
+        {
+            scanningResult = sc.next();  //find and returns the next complete token from this scanner
+            switch(counter){
+                case 2: title = scanningResult;
+                    int beginningIndex = title.indexOf(find);
+                    if(beginningIndex>0){ // it's a series
+                        series = title.substring(0, beginningIndex - 2);
+                        if(Character.isDigit(title.charAt(beginningIndex+5))){ //tom XX
+                            if(title.charAt(beginningIndex+4) == '0') //tom 0X
+                                seriesVolume = Integer.parseInt(title.substring(beginningIndex+5, beginningIndex + 6));
+                            else {
+                                seriesVolume = Integer.parseInt(title.substring(beginningIndex+4, beginningIndex+6));
+                            }
+                            title = title.substring(beginningIndex+8);
+                        }
+                        else{ //tom X
+                            seriesVolume = Integer.parseInt(title.substring(beginningIndex+4, beginningIndex + 5));
+                            title = title.substring(beginningIndex+7);
+                        }
+                    }
+                    break;
+                case 3: author = scanningResult; break;
+                case 4: publishYear = Integer.parseInt(scanningResult); break;
+                case 5: pages = Integer.parseInt(scanningResult); break;
+                case 6: genre = scanningResult; break;
+                default: break;
+
+            }
+            counter++;
+            if(counter == 11){ //scanning next line
+                counter = 1;
+                Book nextBook = new Book(title, author, pages, publishYear, genre, series, seriesVolume);
+                books.add(nextBook);
+            }
+
+        }
+        sc.close();  //closes the scanner
+        return books;
+    }
+
+    public static void test() throws FileNotFoundException {
+        ArrayList<Book> books = returnBooksFromFile();
+        for(Book book: books){
+            book.description();
+            System.out.println();
+        }
+    }
+}
